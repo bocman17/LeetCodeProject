ae8991f5a572317dfe8cd3dadf7ad3976676d6ff
Tomas Hejlik <tomas.hejlik@plus4u.net>
2022-10-27 11:44:50 +0200

    added 006

---
+++ LeetCode/0001-0050/006-ZigZagConversion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _006_ZigZagConversion
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows <= 1 || s.Length <= 1) { return s; }
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                int pos = i;
+                bool flag = false;
+
+                while (pos < s.Length)
+                {
+
+                    sb.Append(s[pos]);
+                    if (flag && i != 0 || i == numRows - 1) pos += i * 2;
+                    else pos += (numRows - i + numRows - i - 2);
+
+                    flag = !flag;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ TestProject1/0001-0050/006-ZigZagConversion-Test.cs
@@ -0,0 +1,64 @@
+﻿using LeetCode._0001_0050;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0050
+{
+    [TestFixture]
+    public class _006_ZigZagConversion_Test
+    {
+        [Test]
+        public void ConvertTest_General()
+        {
+            var input = "PAYPALISHIRING";
+
+            var solution = new _006_ZigZagConversion();
+
+            var result = solution.Convert(input, 3);
+            Assert.That(result, Is.EqualTo("PAHNAPLSIIGYIR"));
+
+            result = solution.Convert(input, 4);
+            Assert.That(result, Is.EqualTo("PINALSIGYAHRPI"));
+
+            result = solution.Convert(input, 5);
+            Assert.That(result, Is.EqualTo("PHASIYIRPLIGAN"));
+        }
+
+        [Test]
+        public void ConvertTest_RowNumberEqualOne()
+        {
+            var input = "PAYPALISHIRING";
+
+            var solution = new _006_ZigZagConversion();
+
+            var result = solution.Convert(input, 1);
+            Assert.That(result, Is.EqualTo(input));
+        }
+
+        [Test]
+        public void ConvertTest_RowNumberEqualZero()
+        {
+            var input = "PAYPALISHIRING";
+
+            var solution = new _006_ZigZagConversion();
+
+            var result = solution.Convert(input, 0);
+            Assert.That(result, Is.EqualTo(input));
+        }
+
+        [Test]
+        public void ConvertTest_EmptyString()
+        {
+            var input = string.Empty;
+
+            var solution = new _006_ZigZagConversion();
+
+            var result = solution.Convert(input, 2);
+            Assert.That(result, Is.EqualTo(input));
+        }
+    }
+}
