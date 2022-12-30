ac3b2f6e77cca0306487f221fcd58921eb49f8af
Tomas Hejlik <tomas.hejlik@plus4u.net>
2022-12-30 02:39:23 +0100

    added 007

---
+++ LeetCode/0001-0050/007-ReverseInteger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _007_ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            long result = 0;
+            while (x != 0)
+            {
+                result = (x % 10) + (result * 10);
+                if (result > int.MaxValue || result < int.MinValue) return 0;
+                x /= 10;
+            }
+            return (int)result;
+        }
+    }
+}
---
+++ TestProject1/0001-0050/007-ReverseInteger-Test.cs
@@ -0,0 +1,66 @@
+﻿using LeetCode._0001_0050;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0050
+{
+    [TestFixture]
+    public class _007_ReverseInteger_Test
+    {
+        [Test]
+        public void ReverseTest_Positive()
+        {
+            var solution = new _007_ReverseInteger();
+            var result = solution.Reverse(1);
+            Assert.That(result, Is.EqualTo(1));
+
+            result = solution.Reverse(123);
+            Assert.That(result, Is.EqualTo(321));
+
+            result = solution.Reverse(321);
+            Assert.That(result, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ReverseTest_Negative()
+        {
+            var solution = new _007_ReverseInteger();
+            var result = solution.Reverse(-1);
+            Assert.That(result, Is.EqualTo(-1));
+
+            result = solution.Reverse(-123);
+            Assert.That(result, Is.EqualTo(-321));
+
+            result = solution.Reverse(-321);
+            Assert.That(result, Is.EqualTo(-123));
+        }
+
+        [Test]
+        public void ReverseTest_EqualZero()
+        {
+            var solution = new _007_ReverseInteger();
+            var result = solution.Reverse(0);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ReverseTest_PositiveOverFlow()
+        {
+            var solution = new _007_ReverseInteger();
+            var result = solution.Reverse(1534236469);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ReverseTest_NegativeOverFlow()
+        {
+            var solution = new _007_ReverseInteger();
+            var result = solution.Reverse(-1534236469);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
