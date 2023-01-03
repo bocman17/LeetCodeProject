cb52153d7505b626bed91fa36cb60321f246223d
Tomas Hejlik <tomas.hejlik@plus4u.net>
2023-01-03 15:27:09 +0100

    added 010 Regular expression matching

---
+++ LeetCode/0001-0050/010-RegularExpressionMatching.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _010_RegularExpressionMatching
+    {
+        public bool IsMatch(string s, string p)
+        {
+            bool[][] sp = new bool[s.Length + 1][];
+            for (int i = 0; i < s.Length + 1; i++)
+            {
+                sp[i] = new bool[p.Length + 1];
+            }
+            sp[0][0] = true;
+
+            for (int i = 1; i < sp[0].Length; i++)
+            {
+                if (p[i - 1] == '*')
+                {
+                    sp[0][i] = sp[0][i - 2];
+                }
+            }
+
+            for (int i = 1; i < sp.Length; i++)
+            {
+                for (int j = 1; j < sp[0].Length; j++)
+                {
+                    if (p[j - 1] == '.' || p[j - 1] == s[i - 1])
+                    {
+                        sp[i][j] = sp[i - 1][j - 1];
+                    }
+                    else if (p[j - 1] == '*')
+                    {
+                        sp[i][j] = sp[i][j - 2];
+
+                        if (p[j - 2] == '.' || p[j - 2] == s[i - 1])
+                        {
+                            sp[i][j] = sp[i][j] || sp[i - 1][j];
+                        }
+                    }
+                    else
+                    {
+                        sp[i][j] = false;
+                    }
+                }
+            }
+
+            return sp[s.Length][p.Length];
+        }
+    }
+}
---
+++ TestProject1/0001-0050/010-RegularExpressionMatching-Test.cs
@@ -0,0 +1,135 @@
+﻿using LeetCode._0001_0050;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0050
+{
+    [TestFixture]
+    public class _010_RegularExpressionMatching_Test
+    {
+        [Test]
+        public void IsMatchingTest_CharacterNotMatch()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("aa", "a");
+            Assert.That(result, Is.False);
+
+            result = solution.IsMatch("aaa", "aa");
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsMatchingTest_CharacterMatch()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("aa", "aa");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("abc", "abc");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsMatchingTest_Star_Multiple()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("aa", "a*");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("aaa", "a*");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("aaaa", "b*");
+            Assert.That(result, Is.False);
+
+            result = solution.IsMatch("abcd", "d*");
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsMatchingTest_Star_Zero()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("b", "a*b");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsMatchingTest_MultipleStar()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("aab", "c*a*b");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsMatchingTest_Dot()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("ab", ".b");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("ab", ".");
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsMatchingTest_DotWithStar()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("aa", ".*");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("ab", ".*");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("abcdefg", ".*");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("bbbba", ".*a*a");
+            Assert.That(result, Is.True);
+
+            result = solution.IsMatch("ab", ".*c");
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsMatchingTest_MultipleDot()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("aa", "..");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsMatchingTest_StringNullOrEmpty()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("", ".");
+            Assert.That(result, Is.False);
+
+            result = solution.IsMatch("", ".*");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsMatchingTest_MatcherNullOrEmpty()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("abc", "");
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsMatchingTest_StringAndMatcherBothNullOrEmpty()
+        {
+            var solution = new _010_RegularExpressionMatching();
+            var result = solution.IsMatch("", "");
+            Assert.That(result, Is.True);
+        }
+    }
+}
