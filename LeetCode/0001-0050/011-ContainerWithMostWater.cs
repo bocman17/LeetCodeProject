b11b77b2786c9002cbe8cd3ca40113be9598a13a
Tomas Hejlik <tomas.hejlik@plus4u.net>
2023-01-04 17:30:47 +0100

    added 011-Container with most water

---
+++ LeetCode/0001-0050/011-ContainerWithMostWater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _011_ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int MaxArea = 0, i = 0, j = height.Length - 1;
+            while (i < j)
+            {
+                MaxArea = Math.Max((j - i) * Math.Min(height[j], height[i]), MaxArea);
+                (i, j) = height[j] < height[i] ? (i, j - 1) : (i + 1, j);
+            }
+
+            return MaxArea;
+        }
+    }
+}
