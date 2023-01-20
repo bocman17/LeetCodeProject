2145919104c1e8defee0eb45641ef856488f56f9
Tomas Hejlik <tomas.hejlik@plus4u.net>
2023-01-20 03:48:06 +0100

    added 018

---
+++ LeetCode/0001-0050/018-4Sum.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _018_4Sum
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+            List<int> quad = new List<int>();
+
+            KSum(4, 0, target, nums, quad, result);
+            return result;
+        }
+
+        private void KSum(int k, int start, long target, int[] nums, List<int> quad, IList<IList<int>> result)
+        {
+            if (k > 2)
+            {
+                for (int i = start; i < nums.Length - k + 1; i++)
+                {
+                    if (i > start && nums[i] == nums[i - 1])
+                    {
+                        continue;
+                    }
+                    quad.Add(nums[i]);
+                    KSum(k - 1, i + 1, target - nums[i], nums, quad, result);
+                    quad.RemoveAt(quad.Count - 1);
+                }
+            }
+            else
+            {
+                int l = start;
+                int r = nums.Length - 1;
+                while (l < r)
+                {
+                    int sum = nums[l] + nums[r];
+                    if (sum < target)
+                    {
+                        l++;
+                    }
+                    else if (sum > target)
+                    {
+                        r--;
+                    }
+                    else
+                    {
+                        var temp = new List<int>(quad);
+                        temp.Add(nums[l]);
+                        temp.Add(nums[r]);
+                        result.Add(temp);
+                        l++;
+                        while (l < r && nums[l] == nums[l - 1]) l++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestProject1/0001-0050/018-4Sum-Test.cs
@@ -0,0 +1,120 @@
+﻿using LeetCode._0001_0050;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0050
+{
+    [TestFixture]
+    public class _018_4Sum_Test
+    {
+        [Test]
+        public void FourSumTest()
+        {
+            int[] input = { 1, 0, -1, 0, -2, 2 };
+
+            var solution = new _018_4Sum();
+            var result = solution.FourSum(input, 0);
+
+            Assert.That(result, Has.Count.EqualTo(3));
+            AssertHelper.AssertList(new List<IList<int>>
+            {
+                new List<int> { -2, -1, 1, 2},
+                new List<int> { -2, 0, 0, 2 },
+                new List<int> { -1, 0, 0, 1 }
+            }, result);
+        }
+
+        [Test]
+        public void FourSumTest_2()
+        {
+            int[] input = { 1, 4, -3, 0, 0, 0, 5, 0 };
+
+            var solution = new _018_4Sum();
+            var result = solution.FourSum(input, 0);
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            AssertHelper.AssertList(new List<IList<int>>
+            {
+                new List<int> { 0, 0, 0, 0 }
+            }, result);
+        }
+
+        [Test]
+        public void FourSumTest_Empty()
+        {
+            int[] input = { };
+
+            var solution = new _018_4Sum();
+            var result = solution.FourSum(input, 0);
+
+            Assert.That(result, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        public void FourSumTest_NotEnough()
+        {
+
+            var solution = new _018_4Sum();
+
+            int[] input1 = { -1 };
+            var result = solution.FourSum(input1, 0);
+            Assert.That(result, Has.Count.EqualTo(0));
+
+            int[] input2 = { -1, 0 };
+            result = solution.FourSum(input2, 1);
+            Assert.That(result, Has.Count.EqualTo(0));
+
+            int[] input3 = { -1, 0, 1 };
+            result = solution.FourSum(input3, 1);
+            Assert.That(result, Has.Count.EqualTo(0));
+
+        }
+
+        [Test]
+        public void FourSumTest_AllTheSame()
+        {
+            int[] input = { 0, 0, 0, 0, 0, 0 };
+
+            var solution = new _018_4Sum();
+            var result = solution.FourSum(input, 0);
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            AssertHelper.AssertList(new List<IList<int>>
+            {
+                new List<int> { 0, 0, 0, 0 }
+            }, result);
+
+            result = solution.FourSum(input, 1);
+            Assert.That(result, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        public void FourSumTest_SomeDuplicate()
+        {
+            int[] input = { 1, 0, 0, 1, 0, 0, -1, -1, 0, 0 };
+
+            var solution = new _018_4Sum();
+            var result = solution.FourSum(input, 0);
+
+            Assert.That(result, Has.Count.EqualTo(3));
+            AssertHelper.AssertList(new List<IList<int>>
+            {
+                new List<int> { -1, -1, 1, 1 },
+                new List<int> { -1, 0, 0, 1 },
+                new List<int> { 0, 0, 0, 0 }
+            }, result);
+
+            result = solution.FourSum(input, 1);
+            Assert.That(result, Has.Count.EqualTo(2));
+            AssertHelper.AssertList(new List<IList<int>>
+            {
+                new List<int> { -1, 0, 1, 1 },
+                new List<int> { 0, 0, 0, 1  },
+            }, result);
+        }
+    }
+}
