e2e5964b3d2b6038af0174add9203729eac3e898
Tomas Hejlik <tomas.hejlik@plus4u.net>
2023-01-26 05:25:03 +0100

    added 020

---
+++ LeetCode/0001-0050/020-ValidParentheses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _020_ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(')
+                {
+                    stack.Push(')');
+                    continue;
+                }
+                else if (c == '[')
+                {
+                    stack.Push(']');
+                    continue;
+                }
+                else if (c == '{')
+                {
+                    stack.Push('}');
+                    continue;
+                }
+
+                else if (stack.Count == 0 || c != stack.Pop())
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ TestProject1/0001-0050/020-ValidParentheses-Test.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode._0001_0050;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0050
+{
+    [TestFixture]
+    public class _020_ValidParentheses_Test
+    {
+        [Test]
+        public void IsValidTest()
+        {
+            var solution = new _020_ValidParentheses();
+            Assert.Multiple(() =>
+            {
+                Assert.That(solution.IsValid("()"), Is.True);
+                Assert.That(solution.IsValid("[]"), Is.True);
+                Assert.That(solution.IsValid("{}"), Is.True);
+                Assert.That(solution.IsValid("()[]{}"), Is.True);
+            });
+        }
+
+        [Test]
+        public void IsValidTest_NotValid()
+        {
+            var solution = new _020_ValidParentheses();
+            Assert.Multiple(() =>
+            {
+                Assert.That(solution.IsValid("("), Is.False);
+                Assert.That(solution.IsValid(")"), Is.False);
+                Assert.That(solution.IsValid("["), Is.False);
+                Assert.That(solution.IsValid("]"), Is.False);
+                Assert.That(solution.IsValid("{"), Is.False);
+                Assert.That(solution.IsValid("}"), Is.False);
+                Assert.That(solution.IsValid("(]"), Is.False);
+                Assert.That(solution.IsValid("(}"), Is.False);
+                Assert.That(solution.IsValid("[)"), Is.False);
+                Assert.That(solution.IsValid("[}"), Is.False);
+                Assert.That(solution.IsValid("{)"), Is.False);
+                Assert.That(solution.IsValid("{]"), Is.False);
+                Assert.That(solution.IsValid("{]"), Is.False);
+                Assert.That(solution.IsValid("{((())})"), Is.False);
+            });
+        }
+    }
+}
