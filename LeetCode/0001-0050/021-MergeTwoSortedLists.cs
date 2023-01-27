1b3774e6a1f76b227ff6f176d442dfd7c89b2b94
Tomas Hejlik <tomas.hejlik@plus4u.net>
2023-01-27 15:03:43 +0100

    added 021

---
+++ LeetCode/0001-0050/021-MergeTwoSortedLists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _021_MergeTwoSortedLists
+    {
+        public ListNode MergeTwoSortedLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+            if (list1.val < list2.val)
+            {
+                list1.next = MergeTwoSortedLists(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = MergeTwoSortedLists(list2.next, list1);
+                return list2;
+            }
+        }
+    }
+}
---
+++ TestProject1/0001-0050/021-MergeTwoSortedLists-Test.cs
@@ -0,0 +1,82 @@
+﻿using LeetCode._0001_0050;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0050
+{
+    [TestFixture]
+    public class _021_MergeTwoSortedLists_Test
+    {
+        [Test]
+        public void MergeTwoListsTest()
+        {
+            var list1 = TestHelper.GenerateList(new int[] { 1, 3, 5, 7, 9 });
+            var list2 = TestHelper.GenerateList(new int[] { 2, 4, 6, 8, 10 });
+
+            var solution = new _021_MergeTwoSortedLists();
+            var result = solution.MergeTwoSortedLists(list1, list2);
+
+            AssertHelper.AssertLinkList(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, result);
+        }
+
+        [Test]
+        public void MergeTwoListsTest_List1Longer()
+        {
+            var list1 = TestHelper.GenerateList(new int[] { 1, 3, 5, 7, 9, 11, 13 });
+            var list2 = TestHelper.GenerateList(new int[] { 2, 4, 6, 8, 10 });
+
+            var solution = new _021_MergeTwoSortedLists();
+            var result = solution.MergeTwoSortedLists(list1, list2);
+
+            AssertHelper.AssertLinkList(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13 }, result);
+        }
+
+        [Test]
+        public void MergeTwoListsTest_List2Longer()
+        {
+            var list1 = TestHelper.GenerateList(new int[] { 1, 3, 5, 7, 9 });
+            var list2 = TestHelper.GenerateList(new int[] { 2, 4, 6, 8, 10, 12, 14 });
+
+            var solution = new _021_MergeTwoSortedLists();
+            var result = solution.MergeTwoSortedLists(list1, list2);
+
+            AssertHelper.AssertLinkList(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14 }, result);
+        }
+
+        [Test]
+        public void MergeTwoListsTest_List1Empty()
+        {
+            var list2 = TestHelper.GenerateList(new int[] { 2, 4, 6, 8, 10 });
+
+            var solution = new _021_MergeTwoSortedLists();
+            var result = solution.MergeTwoSortedLists(null, list2);
+
+            AssertHelper.AssertLinkList(new int[] { 2, 4, 6, 8, 10 }, result);
+        }
+
+        [Test]
+        public void MergeTwoListsTest_List2Empty()
+        {
+            var list1 = TestHelper.GenerateList(new int[] { 1, 3, 5, 7, 9 });
+
+            var solution = new _021_MergeTwoSortedLists();
+            var result = solution.MergeTwoSortedLists(list1, null);
+
+            AssertHelper.AssertLinkList(new int[] { 1, 3, 5, 7, 9 }, result);
+        }
+
+        [Test]
+        public void MergeTwoListsTest_BothListEmpty()
+        {
+
+            var solution = new _021_MergeTwoSortedLists();
+            var result = solution.MergeTwoSortedLists(null, null);
+
+            Assert.That(result, Is.Null);
+        }
+    }
+}
