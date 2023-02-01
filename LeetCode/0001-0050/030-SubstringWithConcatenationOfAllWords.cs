5d282a08fcd6904bbb6593a502affed8637005f2
Tomas Hejlik <tomas.hejlik@plus4u.net>
2023-02-01 22:56:07 +0100

    added 030

---
+++ LeetCode/0001-0050/030-SubstringWithConcatenationOfAllWords.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _030_SubstringWithConcatenationOfAllWords
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            List<int> result = new();
+            if(words.Length == 0) { return result; }
+            int wordLength = words[0].Length;
+            Dictionary<string, int> wordCount = words.GroupBy(w => w).ToDictionary(g => g.Key, g => g.Count());
+
+            for (int offset = 0; offset < wordLength; offset++)
+            {
+                Queue<string> queue = new(words.Length + 1);
+                int invalidCounter = 0;
+                for (int i = 0; i < (s.Length - offset) / wordLength; i++)
+                {
+                    string word = s.Substring(offset + i * wordLength, wordLength);
+
+                    if (wordCount.ContainsKey(word))
+                    {
+                        if (--wordCount[word] == -1)
+                            invalidCounter++;
+                        queue.Enqueue(word);
+                    }
+                    else
+                    {
+                        while (queue.Count > 0)
+                            wordCount[queue.Dequeue()]++;
+                        invalidCounter = 0;
+                    }
+
+                    if (queue.Count > words.Length)
+                        if (++wordCount[queue.Dequeue()] == 0)
+                            invalidCounter--;
+
+                    if (invalidCounter == 0 && queue.Count == words.Length)
+                        result.Add(offset + (i - words.Length + 1) * wordLength);
+                }
+
+                while (queue.Count > 0)
+                    wordCount[queue.Dequeue()]++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject1/0001-0050/030-SubstringWithConcatenationOfAllWords-Test.cs
@@ -0,0 +1,68 @@
+﻿using LeetCode._0001_0050;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0050
+{
+    [TestFixture]
+    public class _030_SubstringWithConcatenationOfAllWords_Test
+    {
+        [Test]
+        public void FindSubstringTest()
+        {
+            var solution = new _030_SubstringWithConcatenationOfAllWords();
+            var result = solution.FindSubstring("barfoothefoobarman", new string[] { "foo", "bar" });
+
+            AssertHelper.AssertList(new int[] { 0, 9 }, result);
+        }
+
+        [Test]
+        public void FindSubstringTest_NoMatch()
+        {
+            var solution = new _030_SubstringWithConcatenationOfAllWords();
+            var result = solution.FindSubstring("barfoothefoobarman", new string[] { "foo", "bar", "yeh" });
+
+            AssertHelper.AssertList(new int[] { }, result);
+        }
+
+        [Test]
+        public void FindSubstringTest_WordsEmpty()
+        {
+            var solution = new _030_SubstringWithConcatenationOfAllWords();
+            var result = solution.FindSubstring("barfoothefoobarman", new string[] { });
+
+            AssertHelper.AssertList(new int[] { }, result);
+        }
+
+        [Test]
+        public void FindSubstringTest_AllTheSame()
+        {
+            var solution = new _030_SubstringWithConcatenationOfAllWords();
+            var result = solution.FindSubstring("aaaaa", new string[] { "a" });
+
+            AssertHelper.AssertList(new int[] { 0, 1, 2, 3, 4 }, result);
+        }
+
+        [Test]
+        public void FindSubstringTest_WordsTooLong()
+        {
+            var solution = new _030_SubstringWithConcatenationOfAllWords();
+            var result = solution.FindSubstring("barfoo", new string[] { "foo", "bar", "the" });
+
+            AssertHelper.AssertList(new int[] { }, result);
+        }
+
+        [Test]
+        public void FindSubstringTest_DuplicateWords()
+        {
+            var solution = new _030_SubstringWithConcatenationOfAllWords();
+            var result = solution.FindSubstring("wordgoodgoodgoodbestword", new string[] { "word", "good", "best", "good" });
+
+            AssertHelper.AssertList(new int[] { 8 }, result);
+        }
+    }
+}
